5851d71cbc38958892da40d16668f497790eeee8
MattRodigheri <mrodigheri@gmail.com>
2018-10-17 12:19:06 -0700

    webpack working

---
+++ challenge_1/webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client');
+var DIST_DIR = path.join(__dirname, '/public');
+
+module.exports = {
+    "mode": "development",
+    "entry": "./client/index.jsx",
+    "output": {
+        "path": __dirname+'/static',
+        "filename": "[name].[chunkhash:8].js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
