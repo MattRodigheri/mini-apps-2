8be9cf7b5b1d2023fe3a36422bd6dbaf0c92adfa
MattRodigheri <mrodigheri@gmail.com>
2018-10-18 12:10:48 -0700

    obtaining data within 10 day range

--- challenge_2/client/components/App.jsx
+++ challenge_2/client/components/App.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      bpiData: {}
+    }
   }
 
-
+  componentDidMount() {
+    axios.get('/coinData')
+    .then((response) => {
+      console.log(response.data)
+      this.setState({
+        bpiData: response.data.bpi
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
 
   render() {
     return (
-      <div>Hello World</div>
+      <div>Powered by CoinDesk</div>
     )
   }
 }
--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const request = require('request');
+const moment = require('moment');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static(__dirname + './../public'));
+
+var startDate = moment().subtract(10, 'days').format('YYYY-MM-DD');
+var endDate = moment().format('YYYY-MM-DD');
+console.log(startDate)
+
+app.get('/coinData', (req, res) => {
+  request(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`, function(err, data) {
+    if (err) {
+      res.send(err).status(500);
+      console.log(err)
+    } else {
+      res.send(data.body)
+    }
+  })
+
+})
+
+
+
+
+
 
 app.listen(3000, () => console.log('listening on port 3000!'));
