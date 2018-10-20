03084f8deeb1226652e387f711783c0fd75d6c06
MattRodigheri <mrodigheri@gmail.com>
2018-10-20 10:33:30 -0700

    set up react app with createreactapp

---
+++ challenge_4/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+   super();
+
+   this.state = {
+
+   }
+ }
+  render() {
+    return (
+      <div>
+      Hi
+      </div>
+    );
+  }
+}
+
+export default App;
