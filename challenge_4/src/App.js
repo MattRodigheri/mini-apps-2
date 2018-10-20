95c145ef0fd9dfc1a95e53f352bf137df15b6857
MattRodigheri <mrodigheri@gmail.com>
2018-10-20 12:24:24 -0700

    rendering bombs

--- challenge_4/src/App.js
+++ challenge_4/src/App.js
@@ -6,15 +6,39 @@ class App extends Component {
    super();
 
    this.state = {
-
+     board: [
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0,
+       0,0,0,0,0,0,0,0,0,0
+     ]
    }
+   this.reaveal = this.reveal.bind(this);
  }
+
+ reveal(event) {
+   console.log(event.target.id)
+ }
+
   render() {
-    return (
-      <div>
-      Hi
-      </div>
-    );
+    var randomNums = [];
+    while(randomNums.length < 10){
+      randomNums.push(Math.floor(Math.random()*100) + 1);
+    }
+    return this.state.board.map((button, index) => {
+      for (var i = 0; i < randomNums.length; i++) {
+        if (index === randomNums[i]) {
+          return <button key={index} id={'bomb'} onClick={(event) => this.reveal(event)}>b</button>
+        }
+      }
+      return <button key={index} id={index} onClick={(event) => this.reveal(event)}></button>
+    })
   }
 }
 
